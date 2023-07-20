65c625e962f07b23f45abdbca82d68a4823b89f7
Fellipe_Brandao <fellipesilverio31@hotmail.com>
2023-07-20 00:52:21 -0300

    feat: corrigindo typeorm

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,19 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm'
 
-export default async (host = 'database_finances'): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions()
+
+  if (parseInt(process.env.PRODUCAO)) {
+    return createConnection(
+      Object.assign(defaultOptions, {
+        database: defaultOptions.database,
+      }),
+    )
+  }
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host,
+      host: 'database_finances',
       database: defaultOptions.database,
     }),
   )
